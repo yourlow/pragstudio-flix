58373e20a7e0adb3cb6b1bd72f9c76a465b5fc47
Matthew Holloway <m.holloway@uq.net.au>
2025-09-02 09:14:45 +1000

    halfway through the edit page first video

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root "movies#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movie/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
 end
