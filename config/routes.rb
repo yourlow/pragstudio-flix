7fdc75b07eaaf850323752002aaf88860608c229
Matthew Holloway <m.holloway@uq.net.au>
2025-09-03 08:46:40 +1000

    finished edit part 1

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "movies" => "movies#index"
   get "movie/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  get "movie/:id/edit" => "movies#edit", as: "edit_movie"
 end
