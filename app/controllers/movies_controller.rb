a6edb243fd729d5d9c9fcd251d182ae9d125db5b
Matthew Holloway <m.holloway@uq.net.au>
2025-09-04 08:37:09 +1000

    update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,14 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    movie_params = params.require(:movie).permit(:title, :rating, :total_gross, :description, :released_on)
+
+    @movie.update(movie_params)
+
+    redirect_to @movie
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "movies" => "movies#index"
-  get "movie/:id" => "movies#show", as: "movie"
-  get "movie/:id/edit" => "movies#edit", as: "edit_movie"
+  get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update"
 end
