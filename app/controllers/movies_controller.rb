b3f09ac58fc96c37a193c7aa8f8933a5397dda34
Matthew Holloway <m.holloway@uq.net.au>
2025-09-05 08:51:41 +1000

    destroy action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,6 +29,12 @@ class MoviesController < ApplicationController
     redirect_to @movie
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, status: :see_other
+  end
+
 
   private
 
