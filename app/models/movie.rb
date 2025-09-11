64bc30c58667da00b01f4d010d2baee14ede7a60
Matthew Holloway <m.holloway@uq.net.au>
2025-09-12 08:59:00 +1000

    model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,23 @@
 class Movie < ApplicationRecord
+  RATINGS = %w[G PG PG-13 R NC-17]
+
+
+  validates :title, :released_on, :duration, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
+
+
+  validates :rating, inclusion: { in: RATINGS }
+
+
+
   def flop?
     total_gross < 250_000_000
   end
