454918eb6320baf2186ff43fcd6415f0fb3854e6
Matthew Holloway <m.holloway@uq.net.au>
2025-09-22 08:42:21 +1000

    creating reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
   def index
     @movie = Movie.find(params[:movie_id])
 
     @reviews = @movie.reviews
   end
-
 
   def new
     @movie = Movie.find(params[:movie_id])
@@ -12,11 +13,29 @@ class ReviewsController < ApplicationController
     @review = @movie.reviews.new
   end
 
-
-
   def create
     @movie = Movie.find(params[:movie_id])
 
-    @review = @movie.reviews.create(params[:review])
+    @review = @movie.reviews.new(review_params)
+
+
+    if @review.save
+      redirect_to movie_reviews_path(@movie), notice: "Review successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :stars, :comment)
+  end
+
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
