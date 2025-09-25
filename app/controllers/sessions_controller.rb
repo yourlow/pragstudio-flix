a4d53974211fd1e7c7a3b1cedcaa7447e42eefc4
Matthew Holloway <m.holloway@uq.net.au>
2025-09-25 10:56:43 +1000

    up to destroying user sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}"
+
+    else
+      flash.now[:alert] = "Invalid email/password combination"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "You have been logged out."
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_22_224928) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_24_233359) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -32,6 +32,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_22_224928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
